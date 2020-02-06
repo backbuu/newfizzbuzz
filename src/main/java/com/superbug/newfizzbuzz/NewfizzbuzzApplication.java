f7f27000e7f7c3d3b403d264b0256508ff38cb53
backbuu <backbuu@gmail.com>
2020-02-06 09:38:00 +0700

    [init] Project init]

---
+++ src/main/java/com/superbug/newfizzbuzz/NewfizzbuzzApplication.java
@@ -0,0 +1,13 @@
+package com.superbug.newfizzbuzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewfizzbuzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewfizzbuzzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/superbug/newfizzbuzz/NewfizzbuzzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superbug.newfizzbuzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewfizzbuzzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
